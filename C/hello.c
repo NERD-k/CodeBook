225f7fc7880853fc09a0bc51795df98408097b83
NERD <smallnerd.k@gmail.com>
2018-03-26 03:29:38 +0800

    Add an example of C-Programming-Language

---
+++ C/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(void){
+    char CharData;
+    short ShortData;
+    long LongData;
+    long long LLData;
+    float FloatData;
+    double DoubleData;
+
+    printf("Size of CharData:\t%d\n",CharData);
+    printf("Size of ShortData:\t%d\n",ShortData);
+    printf("Size of LongData:\t%d\n",LongData);
+    printf("Size of LLData:\t%d\n",LLData);
+    printf("Size of FloatData:\t%d\n",FloatData);
+    printf("Size of DoubleData:\t%d\n",DoubleData);
+}
